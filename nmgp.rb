d5d07c8b637e6cc19dd96a6692b630e5b9cb9c74
newtorn <codertorn@gmail.com>
2021-09-27 17:29:59 +0800

    update nmpg version

--- nmgp.rb
+++ nmgp.rb
@@ -1,8 +1,9 @@
 class Nmgp < Formula
   desc "Easily use mitmproxy global http proxy on your macOS with nmgp"
   homepage "https://github.com/newtorn/nmgp.git"
-  url "https://github.com/newtorn/nmgp/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "e86f63d49aa934d4544da73dcbdfebd08e37673bf800e9d307db28042b8d0cfe"
+  url "https://github.com/newtorn/nmgp/archive/refs/tags/1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "a29301a886292187d861b53db83e3acfbeb5175276ec494dd9dac4c96aa6c4d4"
   license "MIT"
 
   def install
@@ -12,6 +13,3 @@ class Nmgp < Formula
   end
 
   test do
-    system "false"
-  end
-end
